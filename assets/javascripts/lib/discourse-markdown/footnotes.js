f70b46ab7d92025fe6502c7fe661206672567f8f
Martin Brennan <martin@discourse.org>
2022-10-10 12:06:55 +1000

    FIX: Do not registerPlugin if no window.markdownitFootnote
    In certain environments (e.g. QUnit tests) window.markdownitFootnote may not
    be present, in this case we do not want to try register the markdown
    plugin since it will result in errors from MarkdownIt.use()

--- assets/javascripts/lib/discourse-markdown/footnotes.js
+++ assets/javascripts/lib/discourse-markdown/footnotes.js
@@ -1,6 +1,7 @@
 export function setup(helper) {
   helper.registerOptions((opts, siteSettings) => {
-    opts.features["footnotes"] = !!siteSettings.enable_markdown_footnotes;
+    opts.features["footnotes"] =
+      window.markdownitFootnote && !!siteSettings.enable_markdown_footnotes;
   });
 
   helper.allowList([
@@ -19,5 +20,7 @@ export function setup(helper) {
     },
   });
 
-  helper.registerPlugin(window.markdownitFootnote);
+  if (window.markdownitFootnote) {
+    helper.registerPlugin(window.markdownitFootnote);
+  }
 }
