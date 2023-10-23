48c41f65ed6999d4e4242afdbafe8e5ea64d74a0
Blake Erickson <o.blakeerickson@gmail.com>
2023-10-23 14:49:02 -0600

    DEV: Add after_initialize for autoloading
    Fix for `uninitialized constant Plugin::Instance::AdminDashboardData`

--- plugin.rb
+++ plugin.rb
@@ -6,8 +6,10 @@
 # authors: Sam Saffron, Vitaly Puzrin
 # url: https://github.com/discourse/discourse-footnote
 
-AdminDashboardData.add_problem_check do
-  I18n.t(
-    "The discourse-footnote plugin has been integrated into discourse core. Please remove the plugin from your app.yml and rebuild your container.",
-  )
+after_initialize do
+  AdminDashboardData.add_problem_check do
+    I18n.t(
+      "The discourse-footnote plugin has been integrated into discourse core. Please remove the plugin from your app.yml and rebuild your container.",
+    )
+  end
 end
